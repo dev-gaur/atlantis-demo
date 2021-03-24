0dbf89d17f8041292095ec0d3cf1995796ee8287
Devang <devang.gaur.7@gmail.com>
2021-03-24 20:53:37 +0530

    demo

--- main.tf
+++ main.tf
@@ -17,11 +17,6 @@ resource "null_resource" "cluster" {
     cluster_instance_ids = join(",", aws_instance.cluster.*.id)
   }
 
-  # Bootstrap script can run on any instance of the cluster
-  # So we just choose the first in this case
-  connection {
-    host = element(aws_instance.cluster.*.public_ip, 0)
-  }
 
   provisioner "remote-exec" {
     # Bootstrap script called with private_ip of each node in the clutser
